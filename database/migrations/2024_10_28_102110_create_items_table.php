d6e7226015de46dceee6cbd5b830d60a4173bdbd
Botir <tiboruz@gmail.com>
2024-12-05 12:22:21 +0500

    item uuid

--- database/migrations/2024_10_28_102110_create_items_table.php
+++ database/migrations/2024_10_28_102110_create_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            //$table->string('uuid')->nullable();
+            $table->string('uuid')->nullable();
             $table->string('name');
            
             $table->string('mark')->nullable();
